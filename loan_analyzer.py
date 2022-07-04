5229038c8da02abb006e8729c5fb88ebbc700c1b
Francisco Franco <franco7@Franciscos-MacBook-Pro.local>
2022-07-03 21:29:50 -0400

    uploading final challenge1

--- loan_analyzer.py
+++ loan_analyzer.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from audioop import avg
+#Need this 2 first lines to use the library
 import csv
 from pathlib import Path
 
@@ -19,22 +19,22 @@ loan_costs = [500, 600, 200, 1000, 450]
 # @TODO: Use the `len` function to calculate the total number of loans in the list.
 # Print the number of loans from the list
 # YOUR CODE HERE!
-count_loans = len(loan_costs)
-print("number of loans",count_loans) 
+number_of_loans = len(loan_costs)
+print(f"The total number of loans is: {number_of_loans}")
 
 # What is the total of all loans?
 # @TODO: Use the `sum` function to calculate the total of all loans in the list.
 # Print the total value of the loans
 # YOUR CODE HERE!
-sum_loans = sum(loan_costs)
-print("sum of loans",sum_loans)
+total_loan_amount = sum(loan_costs)
+print(f"The total of all loans is: ${total_loan_amount}")
 
 # What is the average loan amount from the list?
 # @TODO: Using the sum of all loans and the total number of loans, calculate the average loan price.
 # Print the average loan amount
 # YOUR CODE HERE!
-avg_loan = sum_loans / count_loans
-print("average loan amount",avg_loan)
+average_loan_amount = total_loan_amount / number_of_loans
+print(f"The average loan amount is of: ${average_loan_amount}")
 
 """Part 2: Analyze Loan Data.
 
@@ -45,7 +45,6 @@ Using more detailed data on one of these loans, follow these steps to calculate 
 1. Use get() on the dictionary of additional information to extract the **Future Value** and **Remaining Months** on the loan.
     a. Save these values as variables called `future_value` and `remaining_months`.
     b. Print each variable.
-
     @NOTE:
     **Future Value**: The amount of money the borrower has to pay back upon maturity of the loan (a.k.a. "Face Value")
     **Remaining Months**: The remaining maturity (in months) before the loan needs to be fully repaid.
@@ -70,6 +69,10 @@ loan = {
 # @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
 # Print each variable.
 # YOUR CODE HERE!
+remaining_months = loan.get("remaining_months")
+future_value = loan.get("future_value")
+print(f"The remaining months for the loan are: {remaining_months}")
+print(f"The future value of the loan is: ${future_value}")
 
 
 # @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
@@ -78,13 +81,19 @@ loan = {
 #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
 
 # YOUR CODE HERE!
+hurdle_rate = .20
+present_value = future_value / (1+(hurdle_rate/12)**remaining_months)
+print(f"The present or fair value of the loan is: ${round(present_value, 2)}")
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
 #    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
 #    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
 # YOUR CODE HERE!
-
+if present_value >= loan.get("loan_price"):
+    print("The loan is worth at least the cost to buy it.")
+else:
+    print("The loan is too expensive and not worth the price.")
 
 """Part 3: Perform Financial Calculations.
 
@@ -109,13 +118,19 @@ new_loan = {
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
 # YOUR CODE HERE!
-
+def present_value_function(future_value, remaining_months, annual_discount_rate):
+    pv = future_value / (1 + (annual_discount_rate / 12) ** remaining_months)
+    return (pv)
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
-#print(f"The present value of the loan is: {present_value}")
+future_value = new_loan.get("future_value")
+remaining_months = new_loan.get("remaining_months")
+annual_discount_rate = 0.2
 
+present_value_new_loan = present_value_function(future_value, remaining_months, annual_discount_rate)
+print(f"The present value for the new loan would be: ${round(present_value_new_loan, 2)}")
 
 """Part 4: Conditionally filter lists of loans.
 
@@ -157,13 +172,20 @@ loans = [
 
 # @TODO: Create an empty list called `inexpensive_loans`
 # YOUR CODE HERE!
+inexpensive_loans = []
 
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
 # YOUR CODE HERE!
-
+for loan in loans:
+    loan_amount = loan.get("loan_price")
+    if loan_amount <= 500:
+        inexpensive_loans.append(loan)
+    else:
+        print("Loan amount is too high")
+        
 # @TODO: Print the `inexpensive_loans` list
 # YOUR CODE HERE!
-
+print(inexpensive_loans)
 
 """Part 5: Save the results.
 
@@ -183,8 +205,19 @@ Output this list of inexpensive loans to a csv file
 header = ["loan_price", "remaining_months", "repayment_interval", "future_value"]
 
 # Set the output file path
-output_path = Path("inexpensive_loans.csv")
+output_path = Path("inexpensive_loans.csv") #need the first 3 lines of code to do this "Path"
 
 # @TODO: Use the csv library and `csv.writer` to write the header row
 # and each row of `loan.values()` from the `inexpensive_loans` list.
 # YOUR CODE HERE!
+with open(output_path, "w") as csvfile:
+    csv_writer = csv.writer(csvfile, delimiter=",")
+    csv_writer.writerow(header)
+    for loan in inexpensive_loans:
+        csv_writer.writerow(loan.values())
+
+#1. Make sure 2 first lines of code are written
+#2. Follow "with open" line"
+#3. Make sure there is a variable created (output_path) and ideally select the folder it will go to.
+
+    
